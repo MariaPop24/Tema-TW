b952e9dfc183bd8100ef564019a34c5e1e406dc9
MariaPop24 <79691904+MariaPop24@users.noreply.github.com>
2021-05-20 01:02:43 +0300

    Update server.js

--- ProiectTW/server.js
+++ ProiectTW/server.js
@@ -1,5 +1,5 @@
 // Import packages
-const express = require("express");
+/*const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -120,4 +120,4 @@ function writeJSONFile(content) {
 
 // Pornim server-ul
 app.listen("3000", () => 
-console.log("Server started at: http://localhost:3000"));
+console.log("Server started at: http://localhost:3000"));*/
